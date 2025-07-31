dc25187b704b971d7b94e27ce934540b6cb1604f
Qile0317 <qile0317@gmail.com>
2025-07-31 16:16:53 -0700

    made skip_if_py_not_installed

--- tests/testthat/helper-testingFunctions.R
+++ tests/testthat/helper-testingFunctions.R
@@ -1,3 +1,13 @@
 getdata <- function(dir, name) {
 	readRDS(paste("testdata/", dir, "/", name, ".rds", sep = "")) # could move testdata 1 dir lvl up nstead
 }
+
+skip_if_py_not_installed <- function(python_packages) {
+	for (python_package in python_packages) {
+		if (!reticulate::py_module_available(python_package)) {
+			testthat::skip(paste(
+				"Required Python Module", python_package, "is not available."
+			))
+		}
+	}
+}
