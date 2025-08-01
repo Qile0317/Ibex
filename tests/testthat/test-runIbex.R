f57dd5dc5b7760d556ff218bf110756255b724c7
Qile0317 <qile0317@gmail.com>
2025-08-01 16:16:57 -0700

    fix runIbex warning and adjust format with styler

--- tests/testthat/test-runIbex.R
+++ tests/testthat/test-runIbex.R
@@ -1,112 +1,128 @@
 # test script for runIbex.R - testcases are NOT comprehensive!
 test_that("runIbex handles incorrect inputs gracefully", {
-  expect_error(runIbex(sc.data = ibex_example, chain = "Middle", method = "encoder"),
-               "'arg' should be one of \"Heavy\", \"Light\"")
-  expect_error(runIbex(sc.data = ibex_example, chain = "Heavy", method = "xyz"),
-               "'arg' should be one of \"encoder\", \"geometric\"")
-  expect_error(runIbex(sc.data = ibex_example, chain = "Heavy", method = "encoder", encoder.model = "ABC"),
-               "'arg' should be one of \"CNN\", \"VAE\", \"CNN.EXP\", \"VAE.EXP\"")
-  expect_error(runIbex(sc.data = ibex_example, chain = "Heavy", method = "encoder", encoder.input = "XYZ"),
-               "arg' should be one of \"atchleyFactors\", \"crucianiProperties\", \"kideraFactors\", \"MSWHIM\", \"tScales\", \"OHE\"")
-  expect_error(runIbex(sc.data = ibex_example, chain = "Heavy", method = "geometric", geometric.theta = "not_numeric"),
-               "non-numeric argument to mathematical function")
+  local_reproducible_output(unicode = FALSE)
+
+  expect_error(
+    runIbex(sc.data = ibex_example, chain = "Middle", method = "encoder", verbose = FALSE),
+    "'arg' should be one of \"Heavy\", \"Light\""
+  )
+  expect_error(
+    runIbex(sc.data = ibex_example, chain = "Heavy", method = "xyz", verbose = FALSE),
+    "'arg' should be one of \"encoder\", \"geometric\""
+  )
+  expect_error(
+    runIbex(sc.data = ibex_example, chain = "Heavy", method = "encoder", encoder.model = "ABC", verbose = FALSE),
+    "'arg' should be one of \"CNN\", \"VAE\", \"CNN.EXP\", \"VAE.EXP\""
+  )
+  expect_error(
+    runIbex(sc.data = ibex_example, chain = "Heavy", method = "encoder", encoder.input = "XYZ", verbose = FALSE),
+    "arg' should be one of \"atchleyFactors\", \"crucianiProperties\", \"kideraFactors\", \"MSWHIM\", \"tScales\", \"OHE\""
+  )
+  expect_error(
+    runIbex(sc.data = ibex_example, chain = "Heavy", method = "geometric", geometric.theta = "not_numeric", verbose = FALSE),
+    "non-numeric argument to mathematical function"
+  )
 })
 
 test_that("runIbex works with Seurat object", {
-
   skip_if_py_not_installed(c("keras", "numpy"))
 
   suppressWarnings(sc_example <- CreateSeuratObject(counts = matrix(rnorm(1000), nrow = 10, ncol = 100)))
   sc_example[["CTaa"]] <- sample(c("CASSL", "CASST", NA, "NA_IGHV1", "None_IGHV2"), 100, replace = TRUE)
   sc_example[["CTgene"]] <- sample(c("NA_IGHV1.IGD1.IGJ1.IGM", "NA_IGHV1.IGD1.IGJ1.IGM", NA, "NA_IGHV1.IGD1.IGJ1.IGM", "None_IGHV1.IGD1.IGJ1.IGM"), 100, replace = TRUE)
-  
-  result <- runIbex(sc_example, 
-                    chain = "Heavy", 
-                    method = "encoder",
-                    encoder.model = "VAE", 
-                    encoder.input = "atchleyFactors",
-                    reduction.name = "IbexTest", 
-                    verbose = FALSE)
-  
+
+  result <- runIbex(sc_example,
+    chain = "Heavy",
+    method = "encoder",
+    encoder.model = "VAE",
+    encoder.input = "atchleyFactors",
+    reduction.name = "IbexTest",
+    verbose = FALSE
+  )
+
   expect_true("IbexTest" %in% names(result@reductions))
   expect_true(inherits(result, "Seurat"))
 })
 
 test_that("runIbex works with geometric method", {
-
   skip_if_py_not_installed(c("keras", "numpy"))
 
   sc_example <- suppressWarnings(SeuratObject::CreateSeuratObject(counts = matrix(rnorm(1000), nrow = 10, ncol = 100)))
   sc_example[["CTaa"]] <- sample(c("CASSL", "CASST", NA, "NA_IGHV1", "None_IGHV2"), 100, replace = TRUE)
   sc_example[["CTgene"]] <- sample(c("NA_IGHV1.IGD1.IGJ1.IGM", "NA_IGHV1.IGD1.IGJ1.IGM", NA, "NA_IGHV1.IGD1.IGJ1.IGM", "None_IGHV1.IGD1.IGJ1.IGM"), 100, replace = TRUE)
-  
-  result <- runIbex(sc_example, 
-                    chain = "Heavy", 
-                    method = "geometric",
-                    geometric.theta = pi / 4, 
-                    reduction.name = "IbexGeo", 
-                    verbose = FALSE)
-  
+
+  result <- runIbex(sc_example,
+    chain = "Heavy",
+    method = "geometric",
+    geometric.theta = pi / 4,
+    reduction.name = "IbexGeo",
+    verbose = FALSE
+  )
+
   expect_true("IbexGeo" %in% names(result@reductions))
   expect_true(inherits(result, "Seurat"))
 })
 
 test_that("runIbex filters cells correctly", {
-
   skip_if_py_not_installed(c("keras", "numpy"))
 
   sc_example <- suppressWarnings(CreateSeuratObject(counts = matrix(rnorm(1000), nrow = 10, ncol = 100)))
   sc_example[["CTaa"]] <- c(rep("CASSL", 50), rep(NA, 50))
   sc_example[["CTgene"]] <- sample(c("NA_IGHV1.IGD1.IGJ1.IGM", "NA_IGHV1.IGD1.IGJ1.IGM", NA, "NA_IGHV1.IGD1.IGJ1.IGM", "None_IGHV1.IGD1.IGJ1.IGM"), 100, replace = TRUE)
-  result <- runIbex(sc_example, 
-                    chain = "Heavy", 
-                    method = "encoder",
-                    encoder.model = "VAE", 
-                    encoder.input = "atchleyFactors",
-                    reduction.name = "IbexFiltered", 
-                    verbose = FALSE)
-  
+  result <- runIbex(sc_example,
+    chain = "Heavy",
+    method = "encoder",
+    encoder.model = "VAE",
+    encoder.input = "atchleyFactors",
+    reduction.name = "IbexFiltered",
+    verbose = FALSE
+  )
+
   expect_true("IbexFiltered" %in% names(result@reductions))
-  expect_lt(ncol(result), 100)  # Ensures some cells were filtered out
+  expect_lt(ncol(result), 100) # Ensures some cells were filtered out
 })
 
 test_that("runIbex stops if amino acid sequences are missing", {
-
   skip_if_py_not_installed(c("keras", "numpy"))
 
   sc_example <- suppressWarnings(SeuratObject::CreateSeuratObject(counts = matrix(rnorm(1000), nrow = 10, ncol = 100)))
-  
-  expect_error(runIbex(sc_example, 
-                        chain = "Heavy", 
-                        method = "encoder",
-                        encoder.model = "VAE", 
-                        encoder.input = "atchleyFactors", 
-                        verbose = FALSE),
-                "Amino acid sequences are not added to the single-cell object correctly.")
+
+  expect_error(
+    runIbex(sc_example,
+      chain = "Heavy",
+      method = "encoder",
+      encoder.model = "VAE",
+      encoder.input = "atchleyFactors",
+      verbose = FALSE
+    ),
+    "Amino acid sequences are not added to the single-cell object correctly."
+  )
 })
 
 test_that("runIbex works with different reduction names", {
-
   skip_if_py_not_installed(c("keras", "numpy"))
 
   sc_example <- suppressWarnings(SeuratObject::CreateSeuratObject(counts = matrix(rnorm(1000), nrow = 10, ncol = 100)))
   sc_example[["CTaa"]] <- sample(c("CASSL", "CASST", NA, "NA_IGHV1", "None_IGHV2"), 100, replace = TRUE)
   sc_example[["CTgene"]] <- sample(c("NA_IGHV1.IGD1.IGJ1.IGM", "NA_IGHV1.IGD1.IGJ1.IGM", NA, "NA_IGHV1.IGD1.IGJ1.IGM", "None_IGHV1.IGD1.IGJ1.IGM"), 100, replace = TRUE)
-  result1 <- runIbex(sc_example, 
-                      chain = "Heavy", 
-                      method = "encoder",
-                      encoder.model = "VAE", 
-                      encoder.input = "atchleyFactors",
-                      reduction.name = "Ibex1", 
-                      verbose = FALSE)
-  
-  result2 <- runIbex(sc_example, chain = "Heavy", 
-                      method = "encoder",
-                      encoder.model = "VAE", 
-                      encoder.input = "atchleyFactors",
-                      reduction.name = "Ibex2", 
-                      verbose = FALSE)
-  
+  result1 <- runIbex(sc_example,
+    chain = "Heavy",
+    method = "encoder",
+    encoder.model = "VAE",
+    encoder.input = "atchleyFactors",
+    reduction.name = "Ibex1",
+    verbose = FALSE
+  )
+
+  result2 <- runIbex(sc_example,
+    chain = "Heavy",
+    method = "encoder",
+    encoder.model = "VAE",
+    encoder.input = "atchleyFactors",
+    reduction.name = "Ibex2",
+    verbose = FALSE
+  )
+
   expect_true("Ibex1" %in% names(result1@reductions))
   expect_true("Ibex2" %in% names(result2@reductions))
 })
